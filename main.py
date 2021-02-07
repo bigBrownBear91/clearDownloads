685b3880cf3e9542e3cd6487cbb762ce5353a5b2
danilo <d.big.be@gmail.com>
2021-02-07 15:54:48 +0100

    Rename method to list_folder_content_and_delete_it Move functionality of methods log and remove to method_list_folder_content_and_delete_it Give directory and file_age as parameters to script

--- main.py
+++ main.py
@@ -4,22 +4,24 @@ import os
 import logging
 import subprocess
 import datetime
+import argparse
 
 
-def list_folder_content(dir):
+def list_folder_content_and_delete_it(dir):
     for root, dirs, files in os.walk(dir):
         for d in dirs:
-            list_folder_content(d)
-            log(d)
+            list_folder_content_and_delete_it(d)
+            logging.info(f'REMOVED: file or dir {d} at {datetime.datetime.now()}')
             os.rmdir(d)
 
         for file in files:
             abs_path_file = '/'.join([root, file])
-            if is_file_older_than_four_days(abs_path_file):
-                remove_file(abs_path_file)
+            if is_file_older_than_t_days(abs_path_file):
+                logging.info(f'REMOVED: file or dir {abs_path_file} at {datetime.datetime.now()}')
+                os.remove(abs_path_file)
 
 
-def is_file_older_than_four_days(file):
+def is_file_older_than_t_days(file):
     date_byte = subprocess.check_output(["date", "-r", file, "+%D"])
     date_string = date_byte.decode('utf-8')
     month, day, year = date_string.split('/')
@@ -27,20 +29,18 @@ def is_file_older_than_four_days(file):
     today = datetime.datetime.today().date()
 
     age = (today - date_date).days
-    is_older = True if age > 0 else False
-    return is_older
-
-
-def remove_file(file):
-    log(file)
-    os.remove(file)
-
-
-def log(file):
-    logging.basicConfig(filename=r'/home/danilo/log/clearDownloads', level=logging.INFO)
-    logging.info(f'REMOVED: file or dir {file} at {datetime.datetime.now()}')
+    return age > FILE_AGE
 
 
 if __name__ == '__main__':
-    os.chdir('/home/danilo/Downloads')
-    list_folder_content(os.getcwd())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dir', '--directory', default='/home/danilo/Downloads', type=str, dest='directory')
+    parser.add_argument('-t', '--file-age', default=4, type=int, dest='file_age')
+    args = vars(parser.parse_args())
+
+    DIRECTORY = args['directory']
+    FILE_AGE = args['file_age']
+
+    logging.basicConfig(filename=r'/home/danilo/log/clearDownloads', level=logging.INFO)
+    os.chdir(DIRECTORY)
+    list_folder_content_and_delete_it(os.getcwd())
